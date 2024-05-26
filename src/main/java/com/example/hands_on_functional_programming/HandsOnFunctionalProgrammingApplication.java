3dffb69f73bb856ac52a71a86bde125ead4d1e0e
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-05-26 15:42:40 +0900

    first commit

---
+++ src/main/java/com/example/hands_on_functional_programming/HandsOnFunctionalProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.hands_on_functional_programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnFunctionalProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsOnFunctionalProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hands_on_functional_programming/HandsOnFunctionalProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hands_on_functional_programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOnFunctionalProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
