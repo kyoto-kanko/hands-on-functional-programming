73e5032bc20bd7f5f34992c27b8624f3a06e57c3
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-05-26 16:13:06 +0900

    fix 微修正

--- src/main/java/com/example/hands_on_functional_programming/HandsOnFunctionalProgrammingApplication.java
+++ src/main/java/com/example/hands_on_functional_programming/HandsOnFunctionalProgrammingApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HandsOnFunctionalProgrammingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HandsOnFunctionalProgrammingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HandsOnFunctionalProgrammingApplication.class, args);
+    }
 }
